29de6346b77119c57ac670db9acb7b3904462df5
Simon A. F. Lund <os@safl.dk>
2026-04-05 08:25:34 +0200

    fix: match actual cec-client output format for key events
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- rootfs/home/jellyfin/bin/cec-jellyfin.sh
+++ rootfs/home/jellyfin/bin/cec-jellyfin.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 cec-client -d 1 2>/tmp/cec-bridge.log | while read -r line; do
   case "$line" in
-    *"key: up"*) xdotool key Up ;;
-    *"key: down"*) xdotool key Down ;;
-    *"key: left"*) xdotool key Left ;;
-    *"key: right"*) xdotool key Right ;;
-    *"key: select"*) xdotool key Return ;;
-    *"key: back"*) xdotool key Escape ;;
-    *"key: exit"*) xdotool key Escape ;;
-    *"key: play"*) xdotool key space ;;
-    *"key: pause"*) xdotool key space ;;
-    *"key: stop"*) xdotool key s ;;
+    *"key pressed: up"*) xdotool key Up ;;
+    *"key pressed: down"*) xdotool key Down ;;
+    *"key pressed: left"*) xdotool key Left ;;
+    *"key pressed: right"*) xdotool key Right ;;
+    *"key pressed: select"*) xdotool key Return ;;
+    *"key pressed: exit"*) xdotool key Escape ;;
+    *"key pressed: play"*) xdotool key space ;;
+    *"key pressed: pause"*) xdotool key space ;;
+    *"key pressed: stop"*) xdotool key s ;;
   esac
 done
